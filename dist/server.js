cd85776c4155fd487c8da446e3a576258dd1c7b2
Gabriel593 <gabriel.andrade@outlook.com.br>
2021-05-07 16:37:54 -0300

    Finished the home page

--- dist/server.js
+++ dist/server.js
@@ -1,15 +1,1 @@
-"use strict";
-
-var _express = _interopRequireWildcard(require("express"));
-
-function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function () { return cache; }; return cache; }
-
-function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
-
-const app = (0, _express.default)();
-app.get('/', (req, res) => {
-  return _express.response.json({
-    message: 'Hello World!'
-  });
-});
-app.listen(3333);
+"use strict";console.log("Manoela")
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,3 @@
-import express, { response } from 'express'
-import path from 'path'
-
-const app = express()
-
-app.set('view engine', 'ejs')
-app.set('views', 'views')
-app.use(express.static(path.join(__dirname, '../content')))
-
-app.get('/', (req, res) => {
-  res.render('home')
-})
+import app from './app'
 
 app.listen(3333)
