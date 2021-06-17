b3991fc8d88d2d604230f0630a8a61bc1b4becfc
Gabriel5934 <gabriel.andrade@outlook.com.br>
2021-06-17 11:59:47 -0300

    Setted up chapters navigation

--- functions/src/routes.ts
+++ functions/src/routes.ts
@@ -1,11 +1,25 @@
 import { Router } from 'express'
 
+import {chaptersHashes, chaptersCount} from './models/chapters'
+
 const routes = Router()
 
 // Homepage
 routes.get('/', (req, res) => {
   res.set('Cache-Control', 'public, max-age=300, s-maxage=600')
-  res.render('index')
+  res.render('index', {
+    chaptersCount: chaptersCount,
+    chaptersHashes: chaptersHashes
+  })
 })
+
+routes.get('/capitulo/*', (req, res) => {
+  res.set('Cache-Control', 'public, max-age=300, s-maxage=600')
+
+  res.render('chapter', {
+    chapterHash: chaptersHashes // TODO
+  })
+})
+
 
 export default routes
