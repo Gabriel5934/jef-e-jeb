a9e9dafcb8a551ce6c8116eda1de601ebc6423be
Gabriel5934 <gabriel.andrade@outlook.com.br>
2021-06-22 11:10:38 -0300

    Created not found pages and chapters navigation

--- functions/src/models/chapters.ts
+++
@@ -1,8 +0,0 @@
-import * as fs from 'fs'
-import * as path from 'path'
-
-let chaptersHashes = "7tn0chvqwd" // TODO
-
-let chaptersCount = fs.readdirSync(path.join(__dirname, '../../../public/content/images/chapters-covers')).length
-
-export {chaptersHashes, chaptersCount}
--- functions/src/routes.ts
+++ functions/src/routes.ts
@@ -1,25 +1,42 @@
 import { Router } from 'express'
 
-import {chaptersHashes, chaptersCount} from './models/chapters'
+import * as fs from 'fs'
+import * as path from 'path'
 
 const routes = Router()
+const chaptersCount = fs.readdirSync(path.join(__dirname, '../../public/content/images/chapters-covers')).length
+const chapters = [1, 2, 3, 4, 5]
 
 // Homepage
 routes.get('/', (req, res) => {
   res.set('Cache-Control', 'public, max-age=300, s-maxage=600')
   res.render('index', {
     chaptersCount: chaptersCount,
-    chaptersHashes: chaptersHashes
   })
 })
 
+// Capítulos
 routes.get('/capitulo/*', (req, res) => {
   res.set('Cache-Control', 'public, max-age=300, s-maxage=600')
 
-  res.render('chapter', {
-    chapterHash: chaptersHashes // TODO
-  })
+  let requestSegments = req.path.split('/')
+  let requestedChapter : number = +requestSegments[requestSegments.length - 1]
+
+  if (chapters.includes(requestedChapter)) {
+    res.render('chapter', {
+      chapter : requestedChapter,
+      next: chapters.includes(requestedChapter + 1) ? requestedChapter + 1 : false,
+      previous: chapters.includes(requestedChapter - 1) ? requestedChapter - 1 : false
+    })
+  } else {
+    res.render('notFound')
+  }
 })
 
+// 404 
+routes.get('*', (req, res) => {
+  res.status(404).render('404')
+})
 
 export default routes
+
